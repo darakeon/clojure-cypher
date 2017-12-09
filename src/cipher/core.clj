15159cee444840fd0a70aa4cc6515a6dcf07ab11
Dara Keon <lc.sales.duarte@gmail.com>
2017-12-09 16:13:14 -0200

    chore: remove hello world

--- src/cipher/core.clj
+++ src/cipher/core.clj
@@ -1,10 +1,5 @@
 (ns cipher.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn to-int
   "takes a lowercase letter character and returns its position in the alphabet: a = 0, b = 1, etc."
   [letter-char]
@@ -54,3 +49,4 @@
   [letter text]
   (count (filterv #(= letter %) text))
   )
+
