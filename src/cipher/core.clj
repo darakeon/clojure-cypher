a2d3b09aaaaa73b6fbc17ddba5c5bb05f0178b74
Dara Keon <lc.sales.duarte@gmail.com>
2017-12-09 12:32:05 -0200

    conversion: add function char 2 int
    Get letter position in alphabet, as in a = 0, b = 1

--- src/cipher/core.clj
+++ src/cipher/core.clj
@@ -4,3 +4,9 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn to-int
+  "takes a lowercase letter character and returns its position in the alphabet: a = 0, b = 1, etc."
+  [letter-char]
+  (let [ascii-a (int \a)]
+    (- (int letter-char) ascii-a)))
--- test/cipher/core_test.clj
+++ test/cipher/core_test.clj
@@ -1,7 +1,22 @@
 (ns cipher.core-test
   (:require [clojure.test :refer :all]
-            [cipher.core :refer :all]
+            [cipher.core :as core]
             [midje.sweet :refer :all]))
 
 (fact "this will fail"
       2 => 2)
+
+(facts "takes a lowercase letter character and returns its position in the alphabet: a = 0, b = 1, etc"
+       (fact "character a is the first letter, in position 0"
+             (core/to-int \a) => 0)
+       (fact "character b is the second letter, in position 1"
+             (core/to-int \b) => 1)
+
+       (tabular
+         (core/to-int ?char) => ?result
+         ?char ?result
+         \a 0
+         \b 1
+         \c 2
+         \d 3)
+       )
